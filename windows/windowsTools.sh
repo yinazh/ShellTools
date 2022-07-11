a0253fae746f8b2828d503a2d5fc8b8c3d830215
yinaizhong <yaz937231979@163.com>
2022-07-11 16:49:14 +0800

    add window tols

--- windows/windowsTools.sh
+++ windows/windowsTools.sh
@@ -12,22 +12,35 @@ alias openSignDir='start /d/tools/signapk/sda670-new'
 alias openDpan='start /d/'
 alias openProgram='start /d/program'
 alias openDownload='start /c/Users/yinazh/Downloads'
-alias openWorkplace='start /d/workplace'
+alias openWorkplace='start /d/workspace'
 
 
 
 
 ## for Windows 工作目录
-alias toWorkplace='cd /d/workplace'
+alias toWorkplace='cd /d/workspace/'
 #alias toProgram='cd /d/program'
-#alias toShare='cd /d/workplace/shareWindow'
-#alias toMedia='cd /d/workplace/source'
+#alias toShare='cd /d/workspace/shareWindow'
+#alias toMedia='cd /d/workspace/source'
 alias toUICheck='cd /d/tools/uiCheck'
-#alias toLogSpace='cd /d/workplace/logSpace'
+#alias toLogSpace='cd /d/workspace/logSpace'
 #alias toSign='cd /d/tools/signapk/sda670-new'
-#alias toShellSpace='cd /d/workplace/ShellWork'
+alias toShellSpace='cd /d/workspace/code/ShellTools'
 #alias toTools='cd /d/tools'
 alias toDpan='cd /d/'
 #alias toKotlin='cd /d/software/kotlinc/bin'
 #alias toCode='cd /d/code'
+
+
+## windows下开发软件列表
+###下载部分后续完善，此处优先记录，启动
+
+###针对API测试，可使用postman, RunApi
+#alias startRunApi = 'start '
+
+###SwitchHosts
+alias startSwitchHosts='start /d/Program\ Files/SwitchHosts/SwitchHosts.exe'
+
+###Notepad++
+alias startNotepadd='start /d/Program\ Files\ \(x86\)/Notepad++/notepadd++.exe'
 
