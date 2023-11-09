24555e586943e2c1f2fe79105c01c6e58ee7a49f
yinaizhong <yaz937231979@163.com>
2023-11-09 16:27:24 +0800

    update env path and add utools exe

--- env/windowsEnv.sh
+++ env/windowsEnv.sh
@@ -3,5 +3,5 @@
 export ENV_DIR="${CMDER_ROOT}/../../code/ShellTools/env"
 
 ## 获取本机ip
-alias ip='python ${SHELL_DIR}/ip.py'
-alias open='python ${SHELL_DIR}/open.py'
+alias ip='python ${ENV_DIR}/ip.py'
+alias open='python ${ENV_DIR}/open.py'
