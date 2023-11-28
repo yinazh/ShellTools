a8ff0864e1854731026c112149aaaa7fc20e2104
yinaizhong <yaz937231979@163.com>
2023-11-28 15:36:18 +0800

    update user personal toen

--- user.sh
+++ user.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 function user_help(){
-        echo -e "${FONT_GREEN_UNDERLINE}github_personal_token${FONT_RES}: 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"
+	echo -e "${FONT_GREEN_UNDERLINE}github_personal_token${FONT_RES}: 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  (2024年3月1日到期)"
 }
